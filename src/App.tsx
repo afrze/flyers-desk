5c15d4304a695ab8ff6b9b78629ebd64e9d93124
manojsurabathulla <surabathullamanoj.94@gmail.com>
2023-05-23 22:04:44 +0530

    completed login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import DashBoard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<DashBoard />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,43 +1,24 @@
 import { ChangeEvent } from "react";
 
+import { Link } from "react-router-dom";
+import Button from "../../components/Button";
 import Input from "../../components/Input";
 import Text from "../../components/Text";
-import Button from "../../components/Button";
-import { Link } from "react-router-dom";
 
-const Register = () => {
+const Login = () => {
   const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
   };
-
   return (
     <section className="flex flex-col h-[100vh] items-center justify-center">
       <div className="w-full">
         <div className="flex justify-center mb-3">
           <Text type="h1" className="text-primary-500">
-            Registration
+            Login
           </Text>
         </div>
 
         <div className="flex flex-col items-center py-3 px-5">
-          <div className="w-full flex my-3 sm:w-[50%] sm:justify-center">
-            <Input
-              type="text"
-              name="name"
-              placeholder="Enter your name"
-              className="w-full border border-solid rounded-xl py-3 px-5"
-              onChange={changeHandler}
-            />
-          </div>
-          <div className="w-full flex my-3 sm:w-[50%] sm:justify-center">
-            <Input
-              type="empid"
-              name="employeeID"
-              placeholder="Employee ID FEC****"
-              className="w-full border border-solid rounded-xl py-3 px-5"
-              onChange={changeHandler}
-            />
-          </div>
           <div className="w-full flex my-3 sm:w-[50%] sm:justify-center">
             <Input
               type="email"
@@ -56,15 +37,6 @@ const Register = () => {
               onChange={changeHandler}
             />
           </div>
-          <div className="w-full flex my-3 sm:w-[50%] sm:justify-center">
-            <Input
-              type="conformPassword"
-              name="conformPassword"
-              placeholder="Conform Password"
-              className="w-full border border-solid rounded-xl py-3 px-5"
-              onChange={changeHandler}
-            />
-          </div>
 
           <div className="w-full border rounded-2xl mx-4 my-3 sm:w-[50%]">
             <Button type="submit">Submit</Button>
@@ -73,14 +45,14 @@ const Register = () => {
       </div>
       <div className="flex justify-center items-center ">
         <Text className="mr-3 text-bold" type="h3">
-          Already have an account ?
+          Not an user yet ?
         </Text>
-        <Link className="text-sm" to="/login">
-          Login
+        <Link className="text-sm" to="/register">
+          Register
         </Link>
       </div>
     </section>
   );
 };
 
-export default Register;
+export default Login;
