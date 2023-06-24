52c93b160d24868c6b736f93ef5f0e3b7d3e0ebf
manojsurabathulla <surabathullamanoj.94@gmail.com>
2023-06-24 13:35:25 +0530

    completed ui changes with responsive

--- src/components/DeskNavbar/index.tsx
+++ src/components/DeskNavbar/index.tsx
@@ -1,35 +1,31 @@
 import { Link } from "react-router-dom";
 import { DashboardIcon, TicketIcon } from "../../assets/icons";
 import Text from "../Text";
-import { FlyersLogo } from "../../assets";
 
 const DeskNavbar = () => {
   return (
-    <nav>
-      <div className="flex-center justify-center">
-        <FlyersLogo className="hidden md:block w-[60%]" />
-      </div>
-      <div className="mobile-nav md:mobile-nav md:hidden">
-        <ul className="md:block">
+    <>
+      <div className="">
+        <ul className="">
           <li className="flex items-center pt-4 whitespace-nowrap">
             <DashboardIcon className="mr-2" />
-            <Text className=" md:block" children="Dashboard" />
+            <Text className="" children="Dashboard" />
           </li>
           <Link to="/purchase-request">
             <li className="flex items-center pt-4 whitespace-nowrap">
               <TicketIcon className="mr-2" />
-              <Text className=" md:block" children="Purchase Request" />
+              <Text className="" children="Purchase Request" />
             </li>
           </Link>
           <Link to="/issues">
             <li className="flex items-center pt-4 whitespace-nowrap">
               <TicketIcon className="mr-2" />
-              <Text className=" md:block" children="Issues" />
+              <Text className="" children="Issues" />
             </li>
           </Link>
         </ul>
       </div>
-    </nav>
+    </>
   );
 };
 
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,6 +6,7 @@ import Button from "../Button";
 import { FaTimes } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleSideBar } from "../../store/toggleSlice";
+import DeskNavbar from "../DeskNavbar";
 
 const Navbar = () => {
   const { isOpen } = useSelector((store: any) => store?.toggle);
@@ -18,43 +19,45 @@ const Navbar = () => {
           <MobileLogo className="w-[45px] h-auto md:hidden " />
         </Button>
       </div>
-
-      <div
-        className={
-          isOpen
-            ? "mobile-nav-container pt-3"
-            : "pt-3 md:mobile-nav-container md:hidden"
-        }
-      >
-        <div className="md:hidden">
-          <Button onClick={() => dispatch(toggleSideBar())}>
-            <FaTimes />
-          </Button>
-        </div>
+      {isOpen && (
         <div
-          className="mobile-nav md:mobile-nav md:hidden"
-          onClick={() => dispatch(toggleSideBar())}
+          className={
+            isOpen
+              ? "mobile-nav-container pt-3"
+              : "pt-3 md:mobile-nav-container md:hidden block"
+          }
         >
-          <ul className="md:block">
-            <li className="flex items-center pt-4 whitespace-nowrap">
-              <DashboardIcon className="mr-2" />
-              <Text className=" md:block" children="Dashboard" />
-            </li>
-            <Link to="/purchase-request">
+          <div className="md:hidden">
+            <Button onClick={() => dispatch(toggleSideBar())}>
+              <FaTimes />
+            </Button>
+          </div>
+          <div
+            className="mobile-nav md:mobile-nav md:hidden"
+            onClick={() => dispatch(toggleSideBar())}
+          >
+            <ul className="md:block">
               <li className="flex items-center pt-4 whitespace-nowrap">
-                <TicketIcon className="mr-2" />
-                <Text className=" md:block" children="Purchase Request" />
+                <DashboardIcon className="mr-2" />
+                <Text className=" md:block" children="Dashboard" />
               </li>
-            </Link>
-            <Link to="/issues">
-              <li className="flex items-center pt-4 whitespace-nowrap">
-                <TicketIcon className="mr-2" />
-                <Text className=" md:block" children="Issues" />
-              </li>
-            </Link>
-          </ul>
+              <Link to="/purchase-request">
+                <li className="flex items-center pt-4 whitespace-nowrap">
+                  <TicketIcon className="mr-2" />
+                  <Text className=" md:block" children="Purchase Request" />
+                </li>
+              </Link>
+              <Link to="/issues">
+                <li className="flex items-center pt-4 whitespace-nowrap">
+                  <TicketIcon className="mr-2" />
+                  <Text className=" md:block" children="Issues" />
+                </li>
+              </Link>
+            </ul>
+          </div>
         </div>
-      </div>
+      )}
+      <DeskNavbar />
     </nav>
   );
 };
